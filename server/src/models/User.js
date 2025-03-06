8a1f303cf62fac3160da9eaae75388afe9190d80
Dorelys Rodriguez <rodriguezdorelys9@gmail.com>
2025-03-06 09:31:30 +0100

    Added Mongoose schema for users with required fields

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,33 +1,68 @@
 import mongoose from "mongoose";
 
-import validateAllowedFields from "../util/validateAllowedFields.js";
-
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 8 },
+  post_code: { type: String, minLength: 6, maxLength: 6, required: false },
+  city: { type: String, required: false },
+  created_date: { type: Date, default: Date.now },
+  products: [{ type: String }],
 });
 
-const User = mongoose.model("users", userSchema);
-
-export const validateUser = (userObject) => {
+userSchema.methods.validateUser = function (userObject) {
   const errorList = [];
-  const allowedKeys = ["name", "email"];
+  const allowedKeys = [
+    "name",
+    "email",
+    "confirm_email",
+    "password",
+    "confirm_password",
+    "post_code",
+    "city",
+    "created_date",
+    "products",
+  ];
 
-  const validatedKeysMessage = validateAllowedFields(userObject, allowedKeys);
+  const invalidFields = Object.keys(userObject).filter(
+    (key) => !allowedKeys.includes(key),
+  );
 
-  if (validatedKeysMessage.length > 0) {
-    errorList.push(validatedKeysMessage);
+  if (invalidFields.length > 0) {
+    errorList.push(
+      `The following properties are not allowed: ${invalidFields.join(", ")}`,
+    );
   }
 
-  if (userObject.name == null) {
-    errorList.push("name is a required field");
+  if (!userObject.name) {
+    errorList.push("Name is a required field");
   }
 
-  if (userObject.email == null) {
-    errorList.push("email is a required field");
+  if (!userObject.email) {
+    errorList.push("Email is a required field");
+  }
+
+  if (!userObject.confirm_email) {
+    errorList.push("Confirm Email is a required field");
+  } else if (userObject.email !== userObject.confirm_email) {
+    errorList.push("Email and Confirm Email must match");
+  }
+
+  if (!userObject.password) {
+    errorList.push("Password is a required field");
+  } else if (userObject.password.length < 8) {
+    errorList.push("Password must be at least 8 characters long");
+  }
+
+  if (!userObject.confirm_password) {
+    errorList.push("Confirm Password is a required field");
+  } else if (userObject.password !== userObject.confirm_password) {
+    errorList.push("Password and Confirm Password must match");
   }
 
   return errorList;
 };
+
+const User = mongoose.model("users", userSchema);
 
 export default User;
