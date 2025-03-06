1d03c1656bdeab786e6f920e713775e4d7ea805e
Dorelys Rodriguez <rodriguezdorelys9@gmail.com>
2025-03-06 10:18:50 +0100

    Added Mongoose schema for users with required fields

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+import validateAllowedFields from "../util/validateAllowedFields.js";
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -7,7 +9,7 @@ const userSchema = new mongoose.Schema({
   post_code: { type: String, minLength: 6, maxLength: 6, required: false },
   city: { type: String, required: false },
   created_date: { type: Date, default: Date.now },
-  products: [{ type: String }],
+  items: [{ type: String }],
 });
 
 userSchema.methods.validateUser = function (userObject) {
@@ -21,19 +23,12 @@ userSchema.methods.validateUser = function (userObject) {
     "post_code",
     "city",
     "created_date",
-    "products",
+    "items",
   ];
-
-  const invalidFields = Object.keys(userObject).filter(
-    (key) => !allowedKeys.includes(key),
-  );
-
-  if (invalidFields.length > 0) {
-    errorList.push(
-      `The following properties are not allowed: ${invalidFields.join(", ")}`,
-    );
+  const validatedKeysMessage = validateAllowedFields(userObject, allowedKeys);
+  if (validatedKeysMessage.length > 0) {
+    errorList.push(validatedKeysMessage);
   }
-
   if (!userObject.name) {
     errorList.push("Name is a required field");
   }
@@ -41,19 +36,16 @@ userSchema.methods.validateUser = function (userObject) {
   if (!userObject.email) {
     errorList.push("Email is a required field");
   }
-
   if (!userObject.confirm_email) {
     errorList.push("Confirm Email is a required field");
   } else if (userObject.email !== userObject.confirm_email) {
     errorList.push("Email and Confirm Email must match");
   }
-
   if (!userObject.password) {
     errorList.push("Password is a required field");
   } else if (userObject.password.length < 8) {
     errorList.push("Password must be at least 8 characters long");
   }
-
   if (!userObject.confirm_password) {
     errorList.push("Confirm Password is a required field");
   } else if (userObject.password !== userObject.confirm_password) {
