5a74a84dfc7c98f86b910e3aea4c78690e2a354e
starshynova <l.starshynova@gmail.com>
2025-03-10 19:13:15 +0100

    resolve conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
+import Login from "./pages/User/Login";
 import UserList from "./pages/User/UserList";
 import theme from "./components/theme";
 import { ThemeProvider } from "@mui/material/styles";
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/user" element={<UserList />} />
           <Route path="/register" element={<CreateUser />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/items/create" element={<CreateItem />} />
         </Routes>
       </ThemeProvider>
