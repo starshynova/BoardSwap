41f901dde6d52e47e50d111fb77697b5f7ad4c5a
Vlada B <vlada.bessikalo@gmail.com>
2025-03-10 13:46:52 +0100

    chore: change the server file names for clearer work

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 
-import userRouter from "./routes/user.js";
-import itemRouter from "./routes/item.js";
+import userRouter from "./routes/user-route.js";
+import itemRouter from "./routes/item-route.js";
 
 // Create an express server
 const app = express();
