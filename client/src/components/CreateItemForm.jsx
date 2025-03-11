397d4ee39d4305885321b4e0832e05c87e17458a
starshynova <l.starshynova@gmail.com>
2025-03-11 12:58:07 +0100

    added validation on frontend side

--- client/src/components/CreateItemForm.jsx
+++ client/src/components/CreateItemForm.jsx
@@ -6,6 +6,7 @@ import {
   Button,
   Typography,
 } from "@mui/material";
+import { useState} from "react";
 
 const CreateItemForm = () => {
   const itemType = [
@@ -34,6 +35,61 @@ const CreateItemForm = () => {
     },
   };
 
+  const [formData, setFormData] = useState({
+    title: "",
+    price: "",
+    type: "",
+    condition: "",
+    photo: "",
+    description: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const validate = () => {
+    let newErrors = {};
+
+    if (!formData.title.trim()) {
+      newErrors.title = "Title is required field";
+    }
+    if (!formData.price.trim()) {
+      newErrors.price = "Price is required field";
+    }
+    if (formData.price<0) {
+      newErrors.price = "The price should be a positive number or 0"
+    }
+    if (!formData.type.trim()) {
+      newErrors.type = "Category is required field";
+    }
+    if (!formData.condition.trim()) {
+      newErrors.condition = "Condition is required field";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = async () => {
+    if (!validate()) return;
+    try {
+      const response = await fetch("/api/items", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Data sending error");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -58,87 +114,89 @@ const CreateItemForm = () => {
       <TextField
         required
         id="outlined-title"
+        name="title" 
         label="Title"
-        sx={{
-          width: "100%",
-          backgroundColor: "#D6F9FA",
-          borderRadius: "5px",
-          "& .MuiInputBase-input": {
-            color: "#000000",
-          },
-        }}
-        defaultValue=" "
+        sx={inputStyles}
+        onChange={handleChange}
+        value={formData.title}
+        error={!!errors.title}
+        helperText={errors.title}
       />
       <TextField
         required
         id="outlined-price"
+        name="price" 
         label="Price"
-        sx={{
-          width: "100%",
-          backgroundColor: "#D6F9FA",
-          borderRadius: "5px",
-          "& .MuiInputBase-input": {
-            color: "#000000",
-          },
-        }}
+        sx={inputStyles}
         slotProps={{
           input: {
             startAdornment: <InputAdornment position="start">€</InputAdornment>,
           },
         }}
+        onChange={handleChange}
+        value={formData.price}
+        error={!!errors.price}
+        helperText={errors.price}
       />
+
       <TextField
         required
-        id="outlined-select-product-type"
+        id="outlined-select-category"
+        name="type" 
         select
-        label="Product type"
+        label="Category"
         sx={inputStyles}
-        defaultValue="None"
-        helperText="Please select your product type"
+        helperText="Please select the category of your product"
+        onChange={handleChange}
+        value={formData.type}
+        error={!!errors.type}
       >
         {itemType.map((option) => (
-          <MenuItem
-            sx={{ color: "#000000" }}
-            key={option.value}
-            value={option.value}
-          >
+          <MenuItem sx={{color: "#000000"}} key={option.value} value={option.value}>
             {option.label}
           </MenuItem>
         ))}
       </TextField>
+
       <TextField
         required
         id="outlined-select-condition"
+        name="condition" 
         select
         label="Condition"
         sx={inputStyles}
-        defaultValue="None"
         helperText="Please select the condition of the product"
+        onChange={handleChange}
+        value={formData.condition}
+        error={!!errors.condition}
       >
         {itemCondition.map((option) => (
-          <MenuItem
-            sx={{ color: "#000000" }}
-            key={option.value}
-            value={option.value}
-          >
+          <MenuItem sx={{color: "#000000"}} key={option.value} value={option.value}>
             {option.label}
           </MenuItem>
         ))}
       </TextField>
+
       <TextField
         id="outlined-photo"
+        name="photo" 
         label="Photo"
         sx={inputStyles}
-        defaultValue=" "
+        onChange={handleChange}
+        value={formData.photo}
       />
+
       <TextField
         id="outlined-multiline-description"
+        name="description" 
         label="Description"
         sx={inputStyles}
         multiline
         rows={4}
-        defaultValue=" "
+        onChange={handleChange}
+        value={formData.description}
       />
+
       <Button
         variant="contained"
         size="large"
@@ -148,6 +206,7 @@ const CreateItemForm = () => {
           backgroundColor: "#47CAD1",
           borderRadius: "10px",
         }}
+        onClick={handleSubmit}
       >
         Submit
       </Button>
