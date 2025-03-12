fc051619335300706d81d409685af93994931a72
Dorelys Rodriguez <rodriguezdorelys9@gmail.com>
2025-03-12 13:38:15 +0100

    runned autofix and useState deleted

--- client/src/pages/User/CreateUser.jsx
+++ client/src/pages/User/CreateUser.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useForm from "../../hooks/useForm";
 import UserForm from "../../components/UserForm";
