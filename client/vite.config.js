5237a6ff02d1b2826c7660b11875bf2efe93792f
Dorelys Rodriguez <rodriguezdorelys9@gmail.com>
2025-03-16 11:48:56 +0100

    to be able to use the useForm hook from react library.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,28 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-  const backendProxyTarget = env.VITE_BACKEND_URL ?? 'http://localhost:3000';
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+  const backendProxyTarget = env.VITE_BACKEND_URL ?? "http://localhost:3000";
   return {
     plugins: [react()],
     server: {
-      // Specify server port. Note if the port is already being used, 
-      // Vite will automatically try the next available port so this may not be the actual port 
+      // Specify server port. Note if the port is already being used,
+      // Vite will automatically try the next available port so this may not be the actual port
       // the server ends up listening on.
       port: 5173,
 
       // Automatically open the app in the browser on server start.
-      open: '/',
+      open: "/",
 
       // Proxy /api requests to the API server. This will avoid any CORS issues.
       proxy: {
-        '/api': backendProxyTarget,
+        "/api": backendProxyTarget,
       },
+    },
+    optimizeDeps: {
+      include: ["react-hook-form"],
     },
   };
 });
-
