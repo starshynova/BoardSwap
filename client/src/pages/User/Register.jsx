dec74c568bab5f11126ede056f05983f1ef73118
Dorelys Rodriguez <rodriguezdorelys9@gmail.com>
2025-03-27 14:00:38 +0100

    fixed explore button in login and register form

--- client/src/pages/User/Register.jsx
+++ client/src/pages/User/Register.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import CreateUser from "./CreateUser";
 import ExploreGamesButton from "../../components/ExploreGamesButton";
 
@@ -14,47 +14,35 @@ const Register = () => {
         p: 3,
       }}
     >
-      <Box sx={{ textAlign: "center", marginBottom: "24px" }}>
-        <Typography
-          variant="h5"
-          color="text.secondary"
-          sx={{
-            marginBottom: "16px",
-            maxWidth: "600px",
-            margin: "0 auto",
-            marginTop: "50px",
-          }}
-        >
-          Welcome to BoarSwap! Ready to trade, buy, or sell board games and
-          puzzles? Join now and start your adventure!
-        </Typography>
-      </Box>
-
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        alignItems="center"
-        sx={{ marginTop: "-80px" }}
+      <Typography
+        variant="h5"
+        color="text.secondary"
+        sx={{
+          textAlign: "center",
+          marginTop: "32px",
+          marginBottom: "40px",
+          maxWidth: "600px",
+        }}
       >
-        <Grid item xs={12} md={6}>
-          <Box
-            sx={{
-              p: 4,
-              borderRadius: 2,
-              textAlign: "left",
-              width: "100%",
-              mt: -2,
-            }}
-          >
-            <CreateUser />
-          </Box>
-        </Grid>
-      </Grid>
+        Welcome to BoarSwap! Ready to trade, buy, or sell board games and
+        puzzles? Join now and start your adventure!
+      </Typography>
 
-      <Box display="flex" justifyContent="center" gap={2} mt={-2}>
-        <ExploreGamesButton />
+      <Box
+        sx={{
+          p: 4,
+          borderRadius: 2,
+          textAlign: "center",
+          width: "100%",
+          maxWidth: "500px",
+          backgroundColor: "white",
+          marginBottom: "24px",
+        }}
+      >
+        <CreateUser />
       </Box>
+
+      <ExploreGamesButton />
     </Box>
   );
 };
