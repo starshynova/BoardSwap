0d069102a852c58a3c3e6a3adf664c1dbaabca62
starshynova <l.starshynova@gmail.com>
2025-04-01 12:56:50 +0200

    added dialog for successful deleting

--- client/src/components/ItemDetailsForm.jsx
+++ client/src/components/ItemDetailsForm.jsx
@@ -128,32 +128,42 @@ const ItemDetailsForm = ({
           >
             <Box sx={formStyle.dialog}>
               <DialogTitle>Confirm Deletion</DialogTitle>
-              <DialogContent>
-                <DialogContentText color="red" textAlign={"center"}>
-                  Are you sure you want to delete your product? This action
-                  cannot be undone.
-                </DialogContentText>
-              </DialogContent>
-              <DialogActions>
-                <Button
-                  onClick={handleDelete}
-                  variant="contained"
-                  color="red"
-                  size="large"
-                  sx={formStyle.buttonSmall}
-                >
-                  Yes, Delete
-                </Button>
-                <Button
-                  onClick={() => setShowDeleteConfirm(false)}
-                  variant="contained"
-                  color="primary"
-                  sx={formStyle.buttonSmall}
-                  size="large"
-                >
-                  No, Cancel
-                </Button>
-              </DialogActions>
+              {!deleteSuccess ? (
+                <>
+                  <DialogContent>
+                    <DialogContentText color="red" textAlign={"center"}>
+                      Are you sure you want to delete your product? This action
+                      cannot be undone.
+                    </DialogContentText>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button
+                      onClick={handleDelete}
+                      variant="contained"
+                      color="red"
+                      size="large"
+                      sx={formStyle.buttonSmall}
+                    >
+                      Yes, Delete
+                    </Button>
+                    <Button
+                      onClick={() => setShowDeleteConfirm(false)}
+                      variant="contained"
+                      color="primary"
+                      sx={formStyle.buttonSmall}
+                      size="large"
+                    >
+                      No, Cancel
+                    </Button>
+                  </DialogActions>
+                </>
+              ) : (
+                <DialogContent>
+                  <DialogContentText color="green" textAlign={"center"}>
+                    Your product was successfully deleted
+                  </DialogContentText>
+                </DialogContent>
+              )}
             </Box>
           </Dialog>
           <Button
@@ -166,11 +176,6 @@ const ItemDetailsForm = ({
             Edit
           </Button>
         </div>
-      )}
-      {deleteSuccess && (
-        <Typography color="green" sx={{ mt: 2 }}>
-          Item was successfully deleted
-        </Typography>
       )}
     </Box>
   );
