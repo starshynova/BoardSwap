d978ac2e4c1146bcfa2d42605a0fdd8daa15a6d0
Vlada B <vlada.bessikalo@gmail.com>
2025-04-03 13:01:41 +0200

    feature: add navigation to item details from my item

--- client/src/components/UserDashboard/ItemsSection.jsx
+++ client/src/components/UserDashboard/ItemsSection.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import { Card, CardContent, Typography, Button, Box } from "@mui/material";
 import DeleteButton from "../Buttons/DeleteButton";
 import EditButton from "../Buttons/EditButton";
+import { useNavigate } from "react-router-dom";
 
 const ItemsSection = ({
   items,
@@ -10,138 +11,151 @@ const ItemsSection = ({
   handleDeleteClick,
   handleShowMoreItems,
   handleShowLessItems,
-}) => (
-  <Box sx={{ py: "20px", px: "80px" }}>
-    <Typography
-      variant="h5"
-      color="textSecondary"
-      sx={{
-        textAlign: "left",
-        paddingTop: "20px",
-        paddingBottom: "20px",
-      }}
-    >
-      My Items
-    </Typography>
+}) => {
+  const navigate = useNavigate();
 
-    {itemsError && <Typography color="error">{itemsError}</Typography>}
-    {items.length === 0 && !itemsError && (
-      <Typography color="textSecondary" align="center">
-        You have not uploaded any items yet.
+  const handleNavigate = (id) => {
+    navigate(`/items/${id}`);
+  };
+
+  return (
+    <Box sx={{ py: "20px", px: "80px" }}>
+      <Typography
+        variant="h5"
+        color="textSecondary"
+        sx={{
+          textAlign: "left",
+          paddingTop: "20px",
+          paddingBottom: "20px",
+        }}
+      >
+        My Items
       </Typography>
-    )}
 
-    {items.length > 0 && (
-      <>
-        <Box
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            gap: "16px",
-          }}
-        >
-          {" "}
-          {items.slice(0, visibleItems).map((item) => (
-            <Card
-              key={item._id}
-              sx={{
-                flex: "0 1 calc(33.33% - 16px)",
-                display: "flex",
-                flexDirection: "column",
-              }}
-            >
-              <CardContent
+      {itemsError && <Typography color="error">{itemsError}</Typography>}
+      {items.length === 0 && !itemsError && (
+        <Typography color="textSecondary" align="center">
+          You have not uploaded any items yet.
+        </Typography>
+      )}
+
+      {items.length > 0 && (
+        <>
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "16px",
+            }}
+          >
+            {" "}
+            {items.slice(0, visibleItems).map((item) => (
+              <Card
+                key={item._id}
                 sx={{
-                  flexGrow: 1,
+                  flex: "0 1 calc(33.33% - 16px)",
                   display: "flex",
                   flexDirection: "column",
-                  justifyContent: "space-between",
+                  cursor: "pointer",
                 }}
+                onClick={() => handleNavigate(item._id)}
               >
-                <Box sx={{ mb: "16px" }}>
-                  {item.photo && (
-                    <img
-                      src={item.photo}
-                      alt={item.title}
-                      style={{
-                        width: "80px",
-                        height: "80px",
-                        borderRadius: "5px",
-                        objectFit: "cover",
-                        marginBottom: "16px",
-                      }}
-                    />
-                  )}
-                  <Typography color="textSecondary" sx={{ fontWeight: "bold" }}>
-                    {item.title}
-                  </Typography>
-                  <Typography color="textSecondary">
-                    {item.description || "No description available."}
-                  </Typography>
-                  <Typography color="textSecondary">
-                    <strong>Price:</strong> €{item.price}
-                  </Typography>
-                  <Typography color="textSecondary">
-                    <strong>Condition:</strong> {item.condition}
-                  </Typography>
-                  <Typography color="textSecondary">
-                    <strong>Category:</strong> {item.type}
-                  </Typography>
+                <CardContent
+                  sx={{
+                    flexGrow: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Box sx={{ mb: "16px" }}>
+                    {item.photo && (
+                      <img
+                        src={item.photo}
+                        alt={item.title}
+                        style={{
+                          width: "80px",
+                          height: "80px",
+                          borderRadius: "5px",
+                          objectFit: "cover",
+                          marginBottom: "16px",
+                        }}
+                      />
+                    )}
+                    <Typography
+                      color="textSecondary"
+                      sx={{ fontWeight: "bold" }}
+                    >
+                      {item.title}
+                    </Typography>
+                    <Typography color="textSecondary">
+                      {item.description || "No description available."}
+                    </Typography>
+                    <Typography color="textSecondary">
+                      <strong>Price:</strong> €{item.price}
+                    </Typography>
+                    <Typography color="textSecondary">
+                      <strong>Condition:</strong> {item.condition}
+                    </Typography>
+                    <Typography color="textSecondary">
+                      <strong>Category:</strong> {item.type}
+                    </Typography>
 
-                  {item.status === "Sold" ? (
-                    <Typography color="error">
-                      <strong>Status:</strong> Sold
-                    </Typography>
-                  ) : (
-                    <Typography color="success.main">
-                      <strong>Status:</strong> Available
-                    </Typography>
-                  )}
-                </Box>
-                {item.status !== "Sold" && (
-                  <Box sx={{ display: "flex", gap: "16px;" }}>
-                    <DeleteButton onClick={() => handleDeleteClick(item)}>
-                      Delete
-                    </DeleteButton>
-                    <EditButton> Edit </EditButton>
+                    {item.status === "Sold" ? (
+                      <Typography color="error">
+                        <strong>Status:</strong> Sold
+                      </Typography>
+                    ) : (
+                      <Typography color="success.main">
+                        <strong>Status:</strong> Available
+                      </Typography>
+                    )}
                   </Box>
-                )}
-              </CardContent>
-            </Card>
-          ))}
-        </Box>
+                  {item.status !== "Sold" && (
+                    <Box sx={{ display: "flex", gap: "16px;" }}>
+                      <DeleteButton onClick={() => handleDeleteClick(item)}>
+                        Delete
+                      </DeleteButton>
+                      <EditButton> Edit </EditButton>
+                    </Box>
+                  )}
+                </CardContent>
+              </Card>
+            ))}
+          </Box>
 
-        <Box
-          style={{
-            marginTop: "10px",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          {visibleItems < items.length && (
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleShowMoreItems}
-            >
-              Show More
-            </Button>
-          )}
-          {visibleItems > 3 && (
-            <Button
-              variant="outlined"
-              color="secondary"
-              onClick={handleShowLessItems}
-              style={{ marginLeft: "10px" }}
-            >
-              Show Less
-            </Button>
-          )}
-        </Box>
-      </>
-    )}
-  </Box>
-);
+          <Box
+            style={{
+              marginTop: "10px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            {visibleItems < items.length && (
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleShowMoreItems}
+              >
+                Show More
+              </Button>
+            )}
+            {visibleItems > 3 && (
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={handleShowLessItems}
+                style={{ marginLeft: "10px" }}
+              >
+                Show Less
+              </Button>
+            )}
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+};
 
 ItemsSection.propTypes = {
   items: PropTypes.array.isRequired,
